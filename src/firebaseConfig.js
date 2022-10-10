4a99fbb7fc815ac0063893f7476308b4afdc6b6c
Vivek Pandey <56454109+BuTTeRvp@users.noreply.github.com>
2022-10-10 18:25:49 +0530

    Update firebaseConfig.js
    Security

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { collection } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpu2YFv79-R2lNlcfCnQKpTeoDxFD3A8M",
-  authDomain: "test-d4e95.firebaseapp.com",
-  projectId: "test-d4e95",
-  storageBucket: "test-d4e95.appspot.com",
-  messagingSenderId: "992753482621",
-  appId: "1:992753482621:web:fde7d6eb8ad49d72856a14",
-  measurementId: "G-0GWGD9HVF5"
+  
 };
 
 export const app = initializeApp(firebaseConfig);
